e15095a7e00166c6636fb616df0e70d2733cf66f
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2023-03-29 10:07:23 +0300

    Добавьте файлы проекта.

---
+++ лр9/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace лр9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(111, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 162);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(64, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Программа возвела положительные числа в квадрат и отрицательные в куб";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 313);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Методы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ лр9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр9
+{
+    public partial class Form1 : Form
+    {
+        public string line_2 = "Возведение в квадрат положительных чисел: ";
+        public string line_3 = "Возведение в куб отрицательных чисел: ";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Pow()
+        {
+            int[] array = new int[21];
+            int j = -10;
+            for(int i=0; i<array.Length;i++, j++)
+            {
+                array[i] = j;
+            }
+            int h = 0;
+            for(int i=array.Length-11; h<11; i++,h++)
+            {
+                double num = Convert.ToDouble(Math.Pow(array[i], 2));
+                line_2 += Convert.ToString(num) + ", ";
+
+            }
+            int k = -10;
+            for(int i=0; k<0;i++,k++)
+            {
+                double num = Convert.ToDouble(Math.Pow(array[i], 3));
+                line_3 += Convert.ToString(num) + ", ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pow();
+            textBox1.Text = line_2 + ". " + line_3;
+        }
+    }
+}
